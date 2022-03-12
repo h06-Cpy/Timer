ff7b563f890bdb6997620843b3b3584511db20ae
ssj <hyjun0103@gmail.com>
2022-03-12 19:30:47 +0900

    changed some texts and improve the timer

--- js/timer.js
+++ js/timer.js
@@ -1,16 +1,38 @@
 const timerBtn = document.getElementById("timerButton");
-let inputNum = document.querySelector("#inputQuantity").value;
-let period = parseInt(inputNum, 10);
-    
+let inputNum = document.querySelector("input#inputQuantity").value;
+let due = parseInt(inputNum, 10);
+
+let sittingTime = 0;
+let hour;
+let min;
+let sec;
+const clock = document.querySelector("h3#clock");
+let interval;
+let timeOut;
+function showSittingTime(){
+    hour = parseInt(sittingTime/3600);
+    min = parseInt(sittingTime/60);
+    sec = sittingTime%60;
+    clock.innerText = "you are sitting for "+ hour + ":" + min + ":" + sec;
+}
 function handleTimerBtnClick() {
     if(timerBtn.innerText ==="Start!") {
         timerBtn.innerText = "Stop";
-        setInterval(function(){
+        showSittingTime();
+        interval = setInterval(function() {
+            showSittingTime();
+            sittingTime++;
+        }, 1000);
+        timeOut = setTimeout(function(){
             window.open("index.html");
-            window.close()}, 3000);
-        
+            window.close()}, 6000*due);   
+
     }else{
         timerBtn.innerText = "Start!"
+        clearInterval(interval);
+        clearTimeout(timeOut);
+        sittingTime = 0;
+        clock.innerText = "";
     }
 }
 
